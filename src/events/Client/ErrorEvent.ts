3c63715766dd18c17d423fe05007b6df8b93276d
Clover <39840298+SmollClover@users.noreply.github.com>
2022-02-06 19:07:31 +0100

    [Events] Updated Error and Warn to include Client

--- src/events/Client/ErrorEvent.ts
+++ src/events/Client/ErrorEvent.ts
@@ -1,8 +1,7 @@
 import { RunFunction } from '../../interfaces/Event';
-import consola from 'consola';
 
-export const run: RunFunction = async (error) => {
-	consola.error(error);
+export const run: RunFunction = async (client, error) => {
+	client.logger.error(error);
 };
 
 export const name: string = 'error';
--- src/events/Client/WarnEvent.ts
+++ src/events/Client/WarnEvent.ts
@@ -1,8 +1,7 @@
 import { RunFunction } from '../../interfaces/Event';
-import consola from 'consola';
 
-export const run: RunFunction = async (info) => {
-	consola.warn(info);
+export const run: RunFunction = async (client, info) => {
+	client.logger.warn(info);
 };
 
 export const name: string = 'warn';
