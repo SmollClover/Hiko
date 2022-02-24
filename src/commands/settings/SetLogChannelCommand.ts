79ab279e0b894f627fb5cd617a3a5549f3b2e728
Clover <39840298+SmollClover@users.noreply.github.com>
2022-02-24 20:47:06 +0100

    [SetLogChannelCommand] Done

--- src/commands/settings/SetLogChannelCommand.ts
+++ src/commands/settings/SetLogChannelCommand.ts
@@ -1,7 +1,45 @@
-import { ApplicationCommandOptionData, CommandInteraction } from 'discord.js';
+import { ApplicationCommandOptionData, CommandInteraction, TextChannel } from 'discord.js';
 import { RunFunction } from '../../interfaces/Command';
+import { Settings } from '../../interfaces/DB';
 
-export const run: RunFunction = async (client, interaction: CommandInteraction) => {};
+export const run: RunFunction = async (client, interaction: CommandInteraction) => {
+	await interaction.deferReply();
+
+	const SettingsSchema = await client.db.load('settings');
+	const Settings = (await SettingsSchema.findOne({ Guild: interaction.guildId })) as Settings;
+
+	if (!Settings) {
+		await interaction.editReply({
+			embeds: [
+				client.fatalErrorEmbed({
+					title: 'Error encountered',
+					description:
+						'Rolling back Settings to Default and emitting Guild Join Event.\nPlease try again, after the Bot has sent the standard Invitation Message.',
+				}),
+			],
+		});
+		
+		return client.emit('guildCreate', interaction.guild);
+	}
+
+	const channel = interaction.options.get('channel')?.channel as TextChannel;
+	const currentChannel = Settings.LogChannelId;
+
+	if (!channel) {
+		if (!currentChannel)
+			return interaction.editReply({ embeds: [client.errorEmbed({ description: "**There's no Log Channel set**" })] });
+
+		Settings.LogChannelId = '';
+		await SettingsSchema.update({ Guild: interaction.guildId }, { ...Settings });
+
+		return interaction.editReply({ embeds: [client.embed({ description: '**Log Channel removed**' })] });
+	} else {
+		Settings.LogChannelId = channel.id;
+		await SettingsSchema.update({ Guild: interaction.guildId }, { ...Settings });
+
+		return interaction.editReply({ embeds: [client.embed({ description: `**Log Channel set to** <#${channel.id}>` })] });
+	}
+};
 
 export const name: string = 'log';
 export const description: string = 'Set a Channel for the Logs';
@@ -9,7 +47,7 @@ export const options: Array<ApplicationCommandOptionData> = [
 	{
 		type: 'CHANNEL',
 		name: 'channel',
-		description: 'Set a Channel as the Log Channel. Leave empty to echo current Channel.',
+		description: 'Set a Channel as the Log Channel. Leave empty to remove current Log Channel.',
 		channelTypes: ['GUILD_TEXT'],
 	},
 ];
