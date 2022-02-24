cbe9ef510fd5f075479a14c6c96f2101f5a9c808
Clover <39840298+SmollClover@users.noreply.github.com>
2022-02-24 20:21:56 +0100

    [SetModeratorCommand] Removed unused import

--- src/commands/settings/SetModeratorCommand.ts
+++ src/commands/settings/SetModeratorCommand.ts
@@ -1,4 +1,4 @@
-import { ApplicationCommandOptionData, CommandInteraction, Guild } from 'discord.js';
+import { ApplicationCommandOptionData, CommandInteraction } from 'discord.js';
 import { RunFunction } from '../../interfaces/Command';
 
 export const run: RunFunction = async (client, interaction: CommandInteraction) => {
