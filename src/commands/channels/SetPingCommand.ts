cd4a734d0d2c9a5805f3c80cb90d925d9feccff4
Clover <39840298+SmollClover@users.noreply.github.com>
2022-02-27 15:10:16 +0100

    [SetPingCommand] Done

--- src/commands/channels/SetPingCommand.ts
+++ src/commands/channels/SetPingCommand.ts
@@ -1,7 +1,61 @@
-import { ApplicationCommandOptionData, CommandInteraction } from 'discord.js';
+import { ApplicationCommandOptionData, CommandInteraction, GuildMember, TextChannel } from 'discord.js';
 import { RunFunction } from '../../interfaces/Command';
+import { Channels } from '../../interfaces/DB';
 
-export const run: RunFunction = async (client, interaction: CommandInteraction) => {};
+export const run: RunFunction = async (client, interaction: CommandInteraction) => {
+	await interaction.deferReply();
+
+	const ChannelsSchema = await client.db.load('channels');
+	const Channels = (await ChannelsSchema.find({ Guild: interaction.guildId })) as Array<Channels>;
+
+	const channel = interaction.options.get('ticket-channel')?.channel as TextChannel;
+	const user = interaction.options.get('user')?.member as GuildMember;
+	const action = interaction.options.get('action')?.value.toString().toLowerCase();
+
+	const Channel = Channels.find((c) => c.Channel === channel.id);
+	if (!Channel)
+		return interaction.editReply({ embeds: [client.errorEmbed({ description: '**Channel is not a Ticket Channel!**' })] });
+
+	const status = Channel.Pings.includes(user.id);
+
+	if (action === 'add') {
+		if (status)
+			return interaction.editReply({
+				embeds: [client.errorEmbed({ description: '**User will already be pinged**' })],
+			});
+
+		Channel.Pings.push(user.id);
+		await ChannelsSchema.update({ Channel: channel.id }, { ...Channel });
+
+		return interaction.editReply({
+			embeds: [client.embed({ description: '**Added User to the pings**' })],
+		});
+	} else if (action === 'remove') {
+		if (!status)
+			return interaction.editReply({
+				embeds: [client.errorEmbed({ description: "**User already won't be pinged**" })],
+			});
+
+		Channel.Pings.splice(Channel.Pings.indexOf(user.id), 1);
+		await ChannelsSchema.update({ Channel: channel.id }, { ...Channel });
+
+		return interaction.editReply({
+			embeds: [client.embed({ description: '**Removed User from pings**' })],
+		});
+	} else if (!action) {
+		return interaction.editReply({
+			embeds: [
+				client.embed({
+					description: status ? '🟢 **User will be pinged**' : "🔴 **User won't be pinged**",
+				}),
+			],
+		});
+	} else {
+		return interaction.editReply({
+			embeds: [client.errorEmbed({ description: '**Unknown Action**\nOnly `add` or `remove` are allowed as actions' })],
+		});
+	}
+};
 
 export const name: string = 'ping';
 export const description: string = 'Add or Remove Roles to be pinged on Ticket creation';
@@ -21,7 +75,7 @@ export const options: Array<ApplicationCommandOptionData> = [
 	},
 	{
 		type: 'STRING',
-		name: 'add-or-remove',
+		name: 'action',
 		description: 'Whether to add or remove the User from the Ticket Pings. Leave empty to echo Ping Status.',
 	},
 ];
