867364748dd0ff97e9e5becf71b25955a981c438
Clover <39840298+SmollClover@users.noreply.github.com>
2022-02-27 15:25:00 +0100

    [SetTicketNumberCommand] Done

--- src/commands/channels/SetTicketNumberCommand.ts
+++ src/commands/channels/SetTicketNumberCommand.ts
@@ -1,7 +1,38 @@
-import { ApplicationCommandOptionData, CommandInteraction } from 'discord.js';
+import { ApplicationCommandOptionData, CommandInteraction, TextChannel } from 'discord.js';
 import { RunFunction } from '../../interfaces/Command';
+import { Channels } from '../../interfaces/DB';
 
-export const run: RunFunction = async (client, interaction: CommandInteraction) => {};
+export const run: RunFunction = async (client, interaction: CommandInteraction) => {
+	await interaction.deferReply();
+
+	const ChannelsSchema = await client.db.load('channels');
+	const Channels = (await ChannelsSchema.find({ Guild: interaction.guildId })) as Array<Channels>;
+
+	const channel = interaction.options.get('ticket-channel')?.channel as TextChannel;
+	const number = interaction.options.get('number')?.value as number;
+
+	const Channel = Channels.find((c) => c.Channel === channel.id);
+	if (!Channel)
+		return interaction.editReply({ embeds: [client.errorEmbed({ description: '**Channel is not a Ticket Channel!**' })] });
+
+	if (number === undefined) {
+		return interaction.editReply({
+			embeds: [client.embed({ description: `**Current Ticket Number: ${Channel.Number}**` })],
+		});
+	} else {
+		if (number < 0)
+			return interaction.editReply({
+				embeds: [client.errorEmbed({ description: '**Ticket Number must not be below zero!**' })],
+			});
+
+		Channel.Number = number;
+		await ChannelsSchema.update({ Channel: channel.id }, { ...Channel });
+
+		return interaction.editReply({
+			embeds: [client.embed({ description: `**Ticket Number set to ${number}**` })],
+		});
+	}
+};
 
 export const name: string = 'number';
 export const description: string = 'Set / Echo the next Ticket Number';
