2cc659841d575372ae43728e79edd15bb1cbc970
Clover <39840298+SmollClover@users.noreply.github.com>
2022-02-27 15:09:39 +0100

    [InteractionCreateEvent] Fixed Bug Now uses editReply instead of reply when Interaction is deffered

--- src/events/Guild/InteractionCreateEvent.ts
+++ src/events/Guild/InteractionCreateEvent.ts
@@ -9,13 +9,17 @@ export const run: RunFunction = async (client, interaction: Interaction) => {
 		if (!command) return;
 
 		command.run(client, interaction).catch((reason: any) => {
-			interaction.reply({
-				embeds: [
-					client.fatalErrorEmbed({
-						description: `An Error occurred while executing the command:\n\`\`\`typescript\n${reason}\n\`\`\``,
-					}),
-				],
-			});
+			const embeds = [
+				client.fatalErrorEmbed({
+					description: `An Error occurred while executing the command:\n\`\`\`typescript\n${reason}\n\`\`\``,
+				}),
+			];
+
+			if (interaction.deferred) {
+				interaction.editReply({ embeds });
+			} else {
+				interaction.editReply({ embeds });
+			}
 
 			return client.logger.error(reason);
 		});
@@ -24,13 +28,19 @@ export const run: RunFunction = async (client, interaction: Interaction) => {
 		if (!button) return;
 
 		button.run(client, interaction).catch((reason: any) => {
-			interaction.reply({
-				embeds: [
-					client.fatalErrorEmbed({
-						description: `An Error occurred while executing the command:\n\`\`\`typescript\n${reason}\n\`\`\``,
-					}),
-				],
-			});
+			const embeds = [
+				client.fatalErrorEmbed({
+					description: `An Error occurred while executing the command:\n\`\`\`typescript\n${reason}\n\`\`\``,
+				}),
+			];
+
+			if (interaction.deferred) {
+				interaction.editReply({ embeds });
+			} else {
+				interaction.editReply({ embeds });
+			}
+
+			return client.logger.error(reason);
 		});
 	}
 };
