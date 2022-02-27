e2ee3a522fc2912343a048f009d17b6eb063c7b5
Clover <39840298+SmollClover@users.noreply.github.com>
2022-02-27 15:15:29 +0100

    [SetQuoteCommand] Done

--- src/commands/channels/SetQuoteCommand.ts
+++ src/commands/channels/SetQuoteCommand.ts
@@ -1,7 +1,56 @@
-import { ApplicationCommandOptionData, CommandInteraction } from 'discord.js';
+import { ApplicationCommandOptionData, CommandInteraction, TextChannel } from 'discord.js';
 import { RunFunction } from '../../interfaces/Command';
+import { Channels } from '../../interfaces/DB';
 
-export const run: RunFunction = async (client, interaction: CommandInteraction) => {};
+export const run: RunFunction = async (client, interaction: CommandInteraction) => {
+	await interaction.deferReply();
+
+	const ChannelsSchema = await client.db.load('channels');
+	const Channels = (await ChannelsSchema.find({ Guild: interaction.guildId })) as Array<Channels>;
+
+	const channel = interaction.options.get('ticket-channel')?.channel as TextChannel;
+	const action = interaction.options.get('true-or-false')?.value;
+
+	const Channel = Channels.find((c) => c.Channel === channel.id);
+	if (!Channel)
+		return interaction.editReply({ embeds: [client.errorEmbed({ description: '**Channel is not a Ticket Channel!**' })] });
+
+	const status = Channel.Quote;
+
+	if (action === true) {
+		if (status)
+			return interaction.editReply({
+				embeds: [client.errorEmbed({ description: '**Message will already be quoted**' })],
+			});
+
+		Channel.Quote = true;
+		await ChannelsSchema.update({ Channel: channel.id }, { ...Channel });
+
+		return interaction.editReply({
+			embeds: [client.embed({ description: '**Message will now be quoted**' })],
+		});
+	} else if (action === false) {
+		if (!status)
+			return interaction.editReply({
+				embeds: [client.errorEmbed({ description: "**Message already won't be quoted**" })],
+			});
+
+		Channel.Quote = false;
+		await ChannelsSchema.update({ Channel: channel.id }, { ...Channel });
+
+		return interaction.editReply({
+			embeds: [client.embed({ description: "**Message won't be quoted now**" })],
+		});
+	} else if (!action) {
+		return interaction.editReply({
+			embeds: [
+				client.embed({
+					description: status ? '🟢 **Message will be quoted**' : "🔴 **Message won't be quoted**",
+				}),
+			],
+		});
+	}
+};
 
 export const name: string = 'quote';
 export const description: string = 'Enable / Disable the Quoting of the Users Message';
