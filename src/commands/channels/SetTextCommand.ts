878c6ca901af32e98fb280305df3d1db0a1e2e8b
Clover <39840298+SmollClover@users.noreply.github.com>
2022-02-27 15:19:35 +0100

    [SetTextCommand] Done

--- src/commands/channels/SetTextCommand.ts
+++ src/commands/channels/SetTextCommand.ts
@@ -1,7 +1,41 @@
-import { ApplicationCommandOptionData, CommandInteraction } from 'discord.js';
+import { ApplicationCommandOptionData, CommandInteraction, TextChannel } from 'discord.js';
 import { RunFunction } from '../../interfaces/Command';
+import { Channels } from '../../interfaces/DB';
 
-export const run: RunFunction = async (client, interaction: CommandInteraction) => {};
+export const run: RunFunction = async (client, interaction: CommandInteraction) => {
+	await interaction.deferReply();
+
+	const ChannelsSchema = await client.db.load('channels');
+	const Channels = (await ChannelsSchema.find({ Guild: interaction.guildId })) as Array<Channels>;
+
+	const channel = interaction.options.get('ticket-channel')?.channel as TextChannel;
+	const text = interaction.options.get('text')?.value as string;
+
+	const Channel = Channels.find((c) => c.Channel === channel.id);
+	if (!Channel)
+		return interaction.editReply({ embeds: [client.errorEmbed({ description: '**Channel is not a Ticket Channel!**' })] });
+
+	if (text) {
+		if (text.length > 256)
+			return interaction.editReply({
+				embeds: [client.errorEmbed({ description: '**Text must not be longer than 256 characters!**' })],
+			});
+
+		Channel.Text = text;
+		await ChannelsSchema.update({ Channel: channel.id }, { ...Channel });
+
+		return interaction.editReply({
+			embeds: [client.embed({ description: '**Text updated**' })],
+		});
+	} else {
+		Channel.Text = '';
+		await ChannelsSchema.update({ Channel: channel.id }, { ...Channel });
+
+		return interaction.editReply({
+			embeds: [client.embed({ description: '**Text removed**' })],
+		});
+	}
+};
 
 export const name: string = 'text';
 export const description: string = 'Set / Echo the Text for the Ticket';
@@ -16,6 +50,6 @@ export const options: Array<ApplicationCommandOptionData> = [
 	{
 		type: 'STRING',
 		name: 'text',
-		description: 'Set the Text of the Ticket Channel. Leave empty to disable.',
+		description: 'Set the Text of the Ticket Channel [max 256 characters]. Leave empty to disable.',
 	},
 ];
